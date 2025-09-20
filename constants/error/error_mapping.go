264647b6c90b82c845db6fd373888eed879ff0ec
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-20 12:55:52 +0700

    Add error handling for field and field schedule

--- constants/error/error_mapping.go
+++ constants/error/error_mapping.go
@@ -1,7 +1,13 @@
 package error
 
+import (
+	errField "field-service/constants/error/field"
+	errFieldSchedule "field-service/constants/error/fieldSchedule"
+)
+
 func ErrMapping(err error) bool {
-	allErrors := append(GeneralErrors[:], UserErrors[:]...)
+	allErrors := make([]error, 0)
+	allErrors = append(append(GeneralErrors[:], errField.FieldErrors[:]...), errFieldSchedule.FieldScheduleErrors...)
 
 	for _, item := range allErrors {
 		if err.Error() == item.Error() {
