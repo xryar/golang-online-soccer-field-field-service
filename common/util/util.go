51ccf1241062870be9cff802828d9404877fb4c9
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-21 15:58:15 +0700

    feat(pagination): add pagination and SHA256 utility functions

--- common/util/util.go
+++ common/util/util.go
@@ -1,13 +1,83 @@
 package util
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math"
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 
+	"github.com/dustin/go-humanize"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
+
+type PaginationParam struct {
+	Count int64       `json:"count"`
+	Page  int         `json:"page"`
+	Limit int         `json:"limit"`
+	Data  interface{} `json:"data"`
+}
+
+type PaginationResult struct {
+	TotalPage    int         `json:"totalPage"`
+	TotalData    int64       `json:"totalData"`
+	NextPage     *int        `json:"nextPage"`
+	PreviousPage *int        `json:"PreviousPage"`
+	Page         int         `json:"page"`
+	Limit        int         `json:"limit"`
+	Data         interface{} `json:"data"`
+}
+
+func GeneratePagination(params PaginationParam) PaginationResult {
+	totalPage := int(math.Ceil(float64(params.Count) / float64(params.Limit)))
+
+	var (
+		nextPage int
+		prevPage int
+	)
+	if params.Page < totalPage {
+		nextPage = params.Page + 1
+	}
+
+	if params.Page > totalPage {
+		prevPage = params.Page - 1
+	}
+
+	result := PaginationResult{
+		TotalPage:    totalPage,
+		TotalData:    params.Count,
+		NextPage:     &nextPage,
+		PreviousPage: &prevPage,
+		Page:         params.Page,
+		Limit:        params.Limit,
+		Data:         params.Data,
+	}
+
+	return result
+
+}
+
+func GenerateSHA256(inputString string) string {
+	hash := sha256.New()
+	hash.Write([]byte(inputString))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	return hashString
+}
+
+func RupiahFormat(amount *float64) string {
+	stringValue := "0"
+	if amount != nil {
+		humanizeValue := humanize.CommafWithDigits(*amount, 0)
+		stringValue = strings.ReplaceAll(humanizeValue, ",", ".")
+	}
+
+	return fmt.Sprintf("Rp. %s", stringValue)
+}
 
 func BindFromJSON(dest any, filename, path string) error {
 	v := viper.New()
