6c94e40c064320212593bab9e688414ddd50e500
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-23 11:10:15 +0700

    implement FindAllWithPagination method

--- repositories/field/field.go
+++ repositories/field/field.go
@@ -2,8 +2,11 @@ package repositories
 
 import (
 	"context"
+	errWrap "field-service/common/error"
+	errConstant "field-service/constants/error"
 	"field-service/domain/dto"
 	"field-service/domain/models"
+	"fmt"
 
 	"gorm.io/gorm"
 )
@@ -25,7 +28,33 @@ func NewFieldRepository(db *gorm.DB) IFieldRepository {
 	return &FieldRepository{db: db}
 }
 
-func (fr *FieldRepository) FindAllWithPagination(context.Context, *dto.FieldRequestParam) ([]models.Field, int64, error)
+func (fr *FieldRepository) FindAllWithPagination(ctx context.Context, param *dto.FieldRequestParam) ([]models.Field, int64, error) {
+	var (
+		fields []models.Field
+		sort   string
+		total  int64
+	)
+	if param.SortColumn != nil {
+		sort = fmt.Sprintf("%s %s", *param.SortColumn, *param.SortOrder)
+	} else {
+		sort = "created_at desc"
+	}
+
+	limit := param.Limit
+	offset := (param.Page - 1) * limit
+	err := fr.db.WithContext(ctx).Limit(limit).Offset(offset).Order(sort).Find(&fields).Error
+	if err != nil {
+		return nil, 0, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	err = fr.db.WithContext(ctx).Model(&fields).Count(&total).Error
+	if err != nil {
+		return nil, 0, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	return fields, total, nil
+
+}
 
 func (fr *FieldRepository) FindAllWithoutPagination(context.Context) ([]models.Field, error)
 
