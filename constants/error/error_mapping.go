018a132c975fa9893f5ae6e11599044055b7109e
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-29 10:23:38 +0700

    refactor error mapping and enhance registry repository structure

--- constants/error/error_mapping.go
+++ constants/error/error_mapping.go
@@ -3,11 +3,22 @@ package error
 import (
 	errField "field-service/constants/error/field"
 	errFieldSchedule "field-service/constants/error/fieldSchedule"
+	errTime "field-service/constants/error/time"
 )
 
 func ErrMapping(err error) bool {
+	var (
+		GeneralErrors       = GeneralErrors
+		FieldErrors         = errField.FieldErrors
+		FieldScheduleErrors = errFieldSchedule.FieldScheduleErrors
+		TimeErrors          = errTime.TimeErrors
+	)
+
 	allErrors := make([]error, 0)
-	allErrors = append(append(GeneralErrors[:], errField.FieldErrors[:]...), errFieldSchedule.FieldScheduleErrors...)
+	allErrors = append(allErrors, GeneralErrors...)
+	allErrors = append(allErrors, FieldErrors...)
+	allErrors = append(allErrors, FieldScheduleErrors...)
+	allErrors = append(allErrors, TimeErrors...)
 
 	for _, item := range allErrors {
 		if err.Error() == item.Error() {
--- repositories/registry.go
+++ repositories/registry.go
@@ -1,1 +1,35 @@
 package repositories
+
+import (
+	fieldRepo "field-service/repositories/field"
+	fieldScheduleRepo "field-service/repositories/fieldSchedule"
+	timeRepo "field-service/repositories/time"
+
+	"gorm.io/gorm"
+)
+
+type Registry struct {
+	db *gorm.DB
+}
+
+type IRegistryRepository interface {
+	GetField() fieldRepo.IFieldRepository
+	GetFieldSchedule() fieldScheduleRepo.IFieldScheduleRepository
+	GetTime() timeRepo.ITimeRepository
+}
+
+func NewRegistryRepository(db *gorm.DB) IRegistryRepository {
+	return &Registry{db: db}
+}
+
+func (r *Registry) GetField() fieldRepo.IFieldRepository {
+	return fieldRepo.NewFieldRepository(r.db)
+}
+
+func (r *Registry) GetFieldSchedule() fieldScheduleRepo.IFieldScheduleRepository {
+	return fieldScheduleRepo.NewFieldScheduleRepository(r.db)
+}
+
+func (r *Registry) GetTime() timeRepo.ITimeRepository {
+	return timeRepo.NewTimeRepository(r.db)
+}
