5baa5f92d11009e2cf9ab863be328964062eaed0
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-30 11:34:04 +0700

    implement registry service with field, field schedule, and time retrieval methods

--- services/registry.go
+++ services/registry.go
@@ -1,1 +1,39 @@
 package services
+
+import (
+	"field-service/common/gcs"
+	"field-service/repositories"
+	fieldService "field-service/services/field"
+	fieldScheduleService "field-service/services/fieldSchedule"
+	timeService "field-service/services/time"
+)
+
+type Registry struct {
+	repository repositories.IRegistryRepository
+	gcs        gcs.IGCSClient
+}
+
+type IRegistryService interface {
+	GetField() fieldService.IFieldService
+	GetFieldSchedule() fieldScheduleService.IFieldScheduleService
+	GetTime() timeService.ITimeService
+}
+
+func NewRegistryService(repository repositories.IRegistryRepository, gcs gcs.IGCSClient) IRegistryService {
+	return &Registry{
+		repository: repository,
+		gcs:        gcs,
+	}
+}
+
+func (r *Registry) GetField() fieldService.IFieldService {
+	return fieldService.NewFieldService(r.repository, r.gcs)
+}
+
+func (r *Registry) GetFieldSchedule() fieldScheduleService.IFieldScheduleService {
+	return fieldScheduleService.NewFieldScheduleService(r.repository)
+}
+
+func (r *Registry) GetTime() timeService.ITimeService {
+	return timeService.NewTimeService(r.repository)
+}
