84c03cdc273ead4d1ebef00972996501bcc84ef3
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-30 18:51:47 +0700

    feat: add RegistryController with methods to access field, field schedule, and time controllers

--- controllers/registry.go
+++ controllers/registry.go
@@ -1,1 +1,34 @@
 package controllers
+
+import (
+	fieldController "field-service/controllers/field"
+	fieldScheduleController "field-service/controllers/fieldSchedule"
+	timeController "field-service/controllers/time"
+	"field-service/services"
+)
+
+type Registry struct {
+	service services.IRegistryService
+}
+
+type IRegistryController interface {
+	GetField() fieldController.IFieldController
+	GetFieldSchedule() fieldScheduleController.IFieldScheduleController
+	GetTime() timeController.ITimeController
+}
+
+func NewRegistryController(service services.IRegistryService) IRegistryController {
+	return &Registry{service: service}
+}
+
+func (r *Registry) GetField() fieldController.IFieldController {
+	return fieldController.NewFieldController(r.service)
+}
+
+func (r *Registry) GetFieldSchedule() fieldScheduleController.IFieldScheduleController {
+	return fieldScheduleController.NewFieldScheduleController(r.service)
+}
+
+func (r *Registry) GetTime() timeController.ITimeController {
+	return timeController.NewTimeController(r.service)
+}
