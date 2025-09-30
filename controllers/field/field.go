617a9976480952fabdb4c2e1a0363850bfe5e951
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-30 14:05:52 +0700

    implement get all with pagination controller

--- controllers/field/field.go
+++ controllers/field/field.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	errValidation "field-service/common/error"
+	"field-service/common/response"
+	"field-service/domain/dto"
 	"field-service/services"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type FieldController struct {
@@ -23,7 +28,49 @@ func NewFieldController(service services.IRegistryService) IFieldController {
 	return &FieldController{service: service}
 }
 
-func (fc *FieldController) GetAllWithPagination(c *gin.Context) {}
+func (fc *FieldController) GetAllWithPagination(c *gin.Context) {
+	var params dto.FieldRequestParam
+	err := c.ShouldBindQuery(&params)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  c,
+		})
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(params)
+	if err != nil {
+		errMessage := http.StatusText(http.StatusUnprocessableEntity)
+		errResponse := errValidation.ErrValidationResponse(err)
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code:    http.StatusBadRequest,
+			Err:     err,
+			Message: &errMessage,
+			Data:    errResponse,
+			Gin:     c,
+		})
+		return
+	}
+
+	result, err := fc.service.GetField().GetAllPagination(c, &params)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  c,
+		})
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResponse{
+		Code: http.StatusOK,
+		Data: result,
+		Gin:  c,
+	})
+}
 
 func (fc *FieldController) GetAllWithoutPagination(c *gin.Context) {}
 
